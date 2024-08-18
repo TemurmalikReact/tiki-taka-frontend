9e3eb0790b79198e6249dacbf8db36d4a36b6945
temurmalik_abdukodirov <temurmalik_abdukodirov@student.itpu.uz>
2024-08-18 23:09:06 +0300

    Data update 1

--- src/app/components/task-list/task-list.view.js
+++ src/app/components/task-list/task-list.view.js
@@ -3,17 +3,39 @@ import { eventBus } from '../../../utils/event-bus';
 
 export class TaskListView {
   tasks = [
-    { id: 1, ism: "Abduqodir Xoshimov", oyin: 2, gol: 5, assist: 2 },
-    { id: 2, ism: "Xurrambek Abdumutalov", oyin: 2, gol: 2, assist: 4 },
-    { id: 3, ism: "Asadullo Qosirov", oyin: 2, gol: 5, assist: 1 },
-    { id: 4, ism: "Jamshid Mamadaliyev", oyin: 2, gol: 2, assist: 4 },
-    { id: 5, ism: "Akim Umarkulov", oyin: 2, gol: 1, assist: 2 },
-    { id: 6, ism: "Asror Haydarov", oyin: 2, gol: 2, assist: 1 },
-    { id: 7, ism: "Olim Tursunaliyev", oyin: 2, gol: 4, assist: 5 },
-    { id: 8, ism: "Fazliddin Jamoliddinov", oyin: 2, gol: 2, assist: 1 },
-    { id: 9, ism: "Firdavs Husainov", oyin: 2, gol: 4, assist: 0 }
+    { id: 1, ism: "Abdumutalov Xurrambek", oyin: 2, gol: 3, assist: 4 },
+    { id: 2, ism: "Muhammad Mabrur", oyin: 2, gol: 1, assist: 1 },
+    { id: 3, ism: "Xolmirzayev Zafar", oyin: 2, gol: 3, assist: 2 },
+    { id: 4, ism: "Zafarov Nurmuhammad", oyin: 2, gol: 6, assist: 2 },
+    { id: 5, ism: "Noname Ali", oyin: 2, gol: 3, assist: 1 },
+    { id: 6, ism: "Noname Bilol", oyin: 2, gol: 0, assist: 1 },
+    { id: 7, ism: "Husainov Firdavs", oyin: 2, gol: 4, assist: 0 },
+    { id: 8, ism: "Haydarov Asror", oyin: 2, gol: 2, assist: 1 },
+    { id: 9, ism: "Umarkulov Akim", oyin: 2, gol: 2, assist: 1 },
+    { id: 10, ism: "Mamadaliyev Jamshid", oyin: 2, gol: 2, assist: 4 },
+    { id: 11, ism: "Qodirov Asadullo", oyin: 2, gol: 6, assist: 3 },
+    { id: 12, ism: "Shodmonov Behruz", oyin: 2, gol: 1, assist: 0 },
+    { id: 13, ism: "Jamoliddinov Fazliddin", oyin: 2, gol: 5, assist: 3 },
+    { id: 14, ism: "Tursunaliyev Olim", oyin: 2, gol: 9, assist: 8 },
+    { id: 15, ism: "Tillavoldiyev Nomonjon", oyin: 2, gol: 2, assist: 1 },
+    { id: 16, ism: "Ergashev Zuhriddin", oyin: 2, gol: 5, assist: 2 },
+    { id: 17, ism: "Muhtoriddinov Suhrobiddin", oyin: 2, gol: 4, assist: 1 },
+    { id: 18, ism: "Raximov Shoxruh", oyin: 2, gol: 4, assist: 1 },
+    { id: 19, ism: "Boburiy", oyin: 2, gol: 0, assist: 4 },
+    { id: 20, ism: "Urmonov Davlatbek", oyin: 2, gol: 2, assist: 1 },
+    { id: 21, ism: "Kamiljanov Habib", oyin: 2, gol: 1, assist: 0 },
+    { id: 22, ism: "Tursunov Muhammadziyo", oyin: 2, gol: 3, assist: 8 },
+    { id: 23, ism: "Jalilov Qobil", oyin: 2, gol: 2, assist: 1 },
+    { id: 24, ism: "Xoshimov Abduqodir", oyin: 2, gol: 2, assist: 2 },
+    { id: 25, ism: "Alimov Shahzod", oyin: 2, gol: 1, assist: 0 },
+    { id: 26, ism: "Yuldashev Ayubxon", oyin: 2, gol: 6, assist: 1 },
+    { id: 27, ism: "Abdukarimov Feruzbek", oyin: 2, gol: 1, assist: 4 },
+    { id: 28, ism: "Dadaboyev Asadulloh", oyin: 2, gol: 5, assist: 4 },
+    { id: 29, ism: "Saydalixonov Asilxon", oyin: 2, gol: 1, assist: 3 },
+    { id: 30, ism: "Otajonov Ozodbek", oyin: 2, gol: 4, assist: 3 },
+    { id: 31, ism: "Boykiziyev Kamronbek", oyin: 2, gol: 8, assist: 0 }
   ];
-  
+
 
   listSelector = 'task-list';
   childSelector = 'task';
@@ -24,7 +46,7 @@ export class TaskListView {
 
   createTaskElement(taskData, i) {
     const task = document.createElement('div');
-    
+
     task.classList.add(this.childSelector, 'task--row');
     task.setAttribute('task-id', taskData.id);
 
@@ -61,11 +83,11 @@ export class TaskListView {
     this.tasks.sort((a, b) => {
       const totalA = a.gol + a.assist;
       const totalB = b.gol + b.assist;
-      
+
       if (totalA === totalB) {
         return b.gol - a.gol; // Goal = Assist
       }
-      
+
       return totalB - totalA; // Goal + Assist
     }).forEach((taskData, i) => {
       const taskElement = this.createTaskElement(taskData, i);
