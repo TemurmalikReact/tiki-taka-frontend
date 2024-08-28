e17f85d04ff674de7b61dc3aec111622d70fd5c9
temurmalik_abdukodirov <temurmalik_abdukodirov@student.itpu.uz>
2024-08-28 19:39:10 +0300

    data 6

--- src/app/components/task-list/task-list.view.js
+++ src/app/components/task-list/task-list.view.js
@@ -2,7 +2,7 @@ import './task-list.scss';
 import { eventBus } from '../../../utils/event-bus';
 
 export class TaskListView {
-  tasks = [
+ tasks = [
     { id: 1, ism: "Abdumutalov Xurrambek", oyin: 2, gol: 3, assist: 4 },
     { id: 2, ism: "Muhammad Mabrur", oyin: 2, gol: 1, assist: 1 },
     { id: 3, ism: "Xolmirzayev Zafar", oyin: 4, gol: 7, assist: 5 },
@@ -13,31 +13,31 @@ export class TaskListView {
     { id: 8, ism: "Haydarov Asror", oyin: 2, gol: 2, assist: 1 },
     { id: 9, ism: "Umarkulov Akim", oyin: 2, gol: 2, assist: 1 },
     { id: 10, ism: "Mamadaliyev Jamshid", oyin: 2, gol: 2, assist: 4 },
-    { id: 11, ism: "Qodirov Asadullo", oyin: 3, gol: 8, assist: 3 },
+    { id: 11, ism: "Qodirov Asadullo", oyin: 4, gol: 10, assist: 4 },
     { id: 12, ism: "Shodmonov Behruz", oyin: 2, gol: 1, assist: 0 },
-    { id: 13, ism: "Jamoliddinov Fazliddin", oyin: 2, gol: 5, assist: 3 },
+    { id: 13, ism: "Jamoliddinov Fazliddin", oyin: 3, gol: 6, assist: 4 },
     { id: 14, ism: "Tursunaliyev Olim", oyin: 2, gol: 9, assist: 8 },
     { id: 15, ism: "Tillavoldiyev Nomonjon", oyin: 2, gol: 2, assist: 1 },
     { id: 16, ism: "Ergashev Zuhriddin", oyin: 2, gol: 5, assist: 2 },
-    { id: 17, ism: "Muhtoriddinov Suhrobiddin", oyin: 4, gol: 7, assist: 2 },
-    { id: 18, ism: "Raximov Shoxruh", oyin: 4, gol: 14, assist: 5 },
-    { id: 19, ism: "Boburiy", oyin: 4, gol: 3, assist: 8 },
+    { id: 17, ism: "Muhtoriddinov Suhrobiddin", oyin: 5, gol: 8, assist: 3 },
+    { id: 18, ism: "Raximov Shoxruh", oyin: 5, gol: 16, assist: 6 },
+    { id: 19, ism: "Boburiy", oyin: 5, gol: 4, assist: 8 },
     { id: 20, ism: "Urmonov Davlatbek", oyin: 3, gol: 5, assist: 3 },
     { id: 21, ism: "Kamiljanov Habib", oyin: 3, gol: 1, assist: 1 },
-    { id: 22, ism: "Tursunov Muhammadziyo", oyin: 5, gol: 13, assist: 17 },
-    { id: 23, ism: "Jalilov Qobil", oyin: 3, gol: 5, assist: 1 },
+    { id: 22, ism: "Tursunov Muhammadziyo", oyin: 6, gol: 16, assist: 17 },
+    { id: 23, ism: "Jalilov Qobil", oyin: 4, gol: 6, assist: 2 },
     { id: 24, ism: "Xoshimov Abduqodir", oyin: 3, gol: 3, assist: 2 },
-    { id: 25, ism: "Alimov Shahzod", oyin: 3, gol: 3, assist: 2 },
+    { id: 25, ism: "Alimov Shahzod", oyin: 4, gol: 4, assist: 4 },
     { id: 26, ism: "Yuldashev Ayubxon", oyin: 5, gol: 13, assist: 5 },
     { id: 27, ism: "Abdukarimov Feruzbek", oyin: 4, gol: 4, assist: 7 },
-    { id: 28, ism: "Dadaboyev Asadulloh", oyin: 4, gol: 9, assist: 8 },
+    { id: 28, ism: "Dadaboyev Asadulloh", oyin: 5, gol: 10, assist: 10 },
     { id: 29, ism: "Saydalixonov Asilxon", oyin: 3, gol: 1, assist: 4 },
-    { id: 30, ism: "Otajonov Ozodbek", oyin: 3, gol: 5, assist: 4 },
+    { id: 30, ism: "Otajonov Ozodbek", oyin: 4, gol: 6, assist: 5 },
     { id: 31, ism: "Boyqo'ziyev Kamronbek", oyin: 2, gol: 8, assist: 0 },
     { id: 32, ism: "Juraev Shohruh", oyin: 2, gol: 9, assist: 7 },
     { id: 33, ism: "Soyibjonov Usmon", oyin: 2, gol: 4, assist: 2 },
     { id: 34, ism: "Mahmudov G’oibnazar", oyin: 2, gol: 1, assist: 2 },
-    { id: 35, ism: "Raxmonberdiyev Jo’rabek", oyin: 1, gol: 2, assist: 1 },
+    { id: 35, ism: "Raxmonberdiyev Jo’rabek", oyin: 2, gol: 2, assist: 3 },
     { id: 36, ism: "Хабибуллаев Диёрбек", oyin: 1, gol: 1, assist: 0 },
     { id: 37, ism: "Мамасидиков Бекмурод", oyin: 1, gol: 0, assist: 1 },
     { id: 38, ism: "Одилов Шерзод", oyin: 1, gol: 1, assist: 2 },
@@ -52,8 +52,10 @@ export class TaskListView {
     { id: 47, ism: "Tursunov Ayubxon", oyin: 1, gol: 2, assist: 3 },
     { id: 48, ism: "Gʻaniyev Muhammadaziz", oyin: 1, gol: 2, assist: 1 },
     { id: 49, ism: "Mamurov Muhammadyusuf", oyin: 1, gol: 2, assist: 0 },
-    { id: 50, ism: "Raxmonov Elmurod", oyin: 1, gol: 3, assist: 1 },
-    { id: 51, ism: "Артиков Жавохир", oyin: 1, gol: 1, assist: 0 }
+    { id: 50, ism: "Raxmonov Elmurod", oyin: 2, gol: 4, assist: 3 },
+    { id: 51, ism: "Артиков Жавохир", oyin: 2, gol: 2, assist: 0 },
+    { id: 52, ism: "Mominov Yahyo", oyin: 2, gol: 4, assist: 3 },
+    { id: 53, ism: "Qodirjonov Abdulhamid", oyin: 3, gol: 1, assist: 4 }
 ]
 
   listSelector = 'task-list';
